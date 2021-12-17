ba9539548a7c7abf2716fda8def25df607f48d26
Kirill Ivanov <puaktopblm@yandex.ru>
2021-12-18 03:54:41 +0700

    переделал таблицу

--- js/schedule.js
+++ js/schedule.js
@@ -1,19 +1,34 @@
 export async function loadSchedule() {
-	const week = [ "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс",]
-	var app = "https://script.googleusercontent.com/macros/echo?user_content_key=eXv-wBph2KNHJ390_tLAfbUeu7PHI9SrQ2ZuyrkSBF-_gmqjhCatAF34TcBZJiYU6-qQVWzDQIstMsiznBRZ3akSsbWyKPZ0m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIQlvpU1GOeD2VMqcTimNnabnY10Jkyvm6pX2S0otVy9vr-rewZNxc2WgdPf5ainy8_8ky2qSAzTmORYnZkGfjzvRWoe75B8sQ&lib=M_RcEZdeIyVSqki8icMWnjsRCGdEBlw7P",
-		 output = '',
-		 xhr = new XMLHttpRequest();
-	xhr.open('GET', app);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState !== 4) return;
-		if (xhr.status == 200) {
-			var r = JSON.parse(xhr.responseText),
-			result = r["result"][0];
-			console.log(r)
-			result.map((value, index)=>output += `${week[index]} | ${value} <br/><hr/>`);
+	const week = [ "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс",];
+	// const saved = "https://script.google.com/macros/s/AKfycbw6_v16RIkusQnSCdMs0eUnsR5iQ5QRcawNB9p12PynjCD3rkboZ4BydFqn6rLekWnS/exec"
+	var app = "https://script.google.com/macros/s/AKfycbw6_v16RIkusQnSCdMs0eUnsR5iQ5QRcawNB9p12PynjCD3rkboZ4BydFqn6rLekWnS/exec";
+	fetch(app).then((res) => res.json().then(
+		(data)=> {
+			console.log(data)
+			// {"result":{"shedule":["9:45-11:14","16:30-17:45 ГЭС","9:45-11:15 ГЭС","15:00-16:15 ГЭС","16:30-17:30 ГЭС","","16:15-17:30 Гэс"],"updated":"2021-12-17T17:32:21.384Z"}}
+				const result = data.result;
+				console.log(data)
+				console.log(result)
+				const date = new Date(Date.parse(result.updated));
+				var output = "";
+				result.schedule.map((value, index)=>output += `<tr><td>${week[index]} </td><td>${value}</td></tr>`);
+				const table = `
+				<table>
+					<colgroup>
+						<col width="10%" />
+						<col width="80%" />
+					</colgroup>
+					<thead>
+						<tr class="header">
+						<th colspan="2">Последнее обновление: ${date.toLocaleDateString("ru")} в ${date.toLocaleTimeString("ru")}</th>
+						</tr>
+					</thead>
+					<tbody>
+						${output}
+					</tbody>
+				</table>`;
 
-		} 		
-		document.getElementById('info').innerHTML = output;
-	}
-	xhr.send()
+				document.getElementById('info').innerHTML = table;
+			} 		
+	))
 }
